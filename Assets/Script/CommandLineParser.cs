618b6d3fcd772bb40a227eb0c3816a998ccdbf1f
ajwad_imran <ajwadimran7@gmail.com>
2019-02-13 23:08:04 +0500

    Here comes the headless command line tool.

---
+++ Assets/Script/CommandLineParser.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+
+public class CommandLineParser : MonoBehaviour {
+
+    void Awake ()
+    {
+        Debug.Log("App Started.");
+        string[] args = Environment.GetCommandLineArgs();
+        foreach (string arg in args) {
+            Debug.Log(arg);
+        }
+        Debug.Log("Quitting app.");
+        Application.Quit();
+    }
+}
